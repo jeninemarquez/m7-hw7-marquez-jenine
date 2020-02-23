5939fc7f2bfc3f2356ac90e4c38e21602d1f9993
Jenine Marquez <jeninemarquez@jenines-air.attlocal.net>
2020-02-23 15:16:16 -0500

    Add API data to JS file

---
+++ main.js
@@ -0,0 +1,17 @@
+var textContainer = document.querySelectorAll('span');
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //Code Dealing With The API Data Goes Here
+        var city = document.createTextNode(apiResult.name);
+        textContainer[0].appendChild(city);
+
+        var weather = document.createTextNode(apiResult.weather[0].description);
+        textContainer[1].appendChild(weather);
+    }
+};
+xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=33467,us&appid=6efff70fe1477748e31c17d1c504635f', true);
+xmlhttp.send();
